91e593a864ce778aaca3d7e2e9670033c5a1287c
Tufren <novalto723@gmail.com>
2022-09-06 23:29:49 +0700

    Menambahkan kode baru ke dalam Website

--- js/part/Store.js
+++ js/part/Store.js
@@ -1,4 +1,4 @@
-const scriptURL = '<SCRIPT URL>'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyH14N8P2klKzx1JGD_wsQQlW7GaK2JR2UWs6RB952CLGwbUrUR3kCx02wTxX27JTJY/exec'
 const form = document.forms['Store']
 
 export const Store = form.addEventListener('submit', e => {
